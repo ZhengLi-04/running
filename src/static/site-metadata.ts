6c68041c5fa39391ae7c430faf27dd98afa2bab9
Chung LEE <zheng_li@zju.edu.cn>
2026-02-25 00:09:17 +0800

    Fix workflows and site metadata

--- src/static/site-metadata.ts
+++ src/static/site-metadata.ts
@@ -16,21 +16,13 @@ const getBasePath = () => {
 
 const data: ISiteMetadataResult = {
   siteTitle: 'Running Page',
-  siteUrl: 'https://yihong.run',
-  logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTtc69JxHNcmN1ETpMUX4dozAgAN6iPjWalQ&usqp=CAU',
-  description: 'Personal site and blog',
+  siteUrl: 'https://zhengli-04.github.io/running',
+  logo: '',
+  description: 'Zheng running page',
   navLinks: [
     {
       name: 'Summary',
       url: `${getBasePath()}/summary`,
-    },
-    {
-      name: 'Blog',
-      url: 'https://github.com/yihong0618/gitblog',
-    },
-    {
-      name: 'About',
-      url: 'https://github.com/yihong0618/running_page/blob/master/README-CN.md',
     },
   ],
 };
